03f0c41f2ed20647c2976f360ec110015c82da81
luksm770 <lukas@lukassmith.se>
2025-09-22 15:42:52 +0200

    Reads input from .yaml file

---
+++ read_file_v2.py
@@ -0,0 +1,19 @@
+import yaml
+from yaml.loader import SafeLoader
+
+def read_file(filename):
+    """Read yaml file.
+
+    Args:
+        filename (str): Name of the config yaml file
+
+    Returns:
+        A list. Each element is a dictionary, containing information on a MD simulation.
+
+    
+    """
+    with open(filename,"r") as f:
+        data = list(yaml.load_all(f,Loader=SafeLoader))
+        return data
+
+print(read_file("test_file.yaml"))
