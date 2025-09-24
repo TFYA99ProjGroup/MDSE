7f5fb02c992c8163bb20f4f0d7aff88220ed1584
Axel K <axelkemppe@outlook.com>
2025-09-24 10:20:49 +0200

    Returned comments and fixed gitignore

--- mini_script.py
+++ mini_script.py
@@ -6,11 +6,28 @@ from ase.visualize import view
 from ase.md.verlet import VelocityVerlet
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from asap3 import EMT, Trajectory
+# TODO Axel/Oskar: Check if asap is better optimized for all following functions instead of ase.
+# Add more params to createCrystal, fix cubic
+# Maybe move down som imports to __main__
 
-def createCrystal(chem_notation='H', structure='sc', positions=None, a=1.0, b=None, c=None, cubic=True):
-    """Create a crystal or molecule."""
+def createCrystal(chem_notation='H', structure='sc', positions=None, a=3.6, b=None, c=None, cubic=True):
+    """Create the atom or molecule or crystal object from the specified params.
+
+    Key args:
+    chemNotation -- the chemical notation of the object (default: 'H')
+    structure -- The Lattice types or Crystal structure of the object (default: 'sc')
+    positions -- Individual atomic positions (optional argument)
+    a -- lattice constant
+    b -- secondary lattice constant (optional argument)
+    c -- third lattice constant (optional argument)
+    cubic --
+    """
     if positions is None:
-        return bulk(chem_notation, structure, a, b, c)
+        crystal = bulk(chem_notation, structure, a, b, c)
+    else:
+        crystal =  bulk(chem_notation, positions, a, b, c)
+    return crystal
+
 
 def simulateNVE(atoms, timestep, length):
     """Run NVE molecular dynamics."""
@@ -19,6 +36,7 @@ def simulateNVE(atoms, timestep, length):
     dyn.attach(traj.write, interval=10)
     dyn.run(length)
 
+## Patrik grejor
 # Did this to be able to run code in jupyter notebook
 def generateTestCrystal():
     """Generate a demo crystal for testing/visualization."""
@@ -30,5 +48,6 @@ def generateTestCrystal():
     return test_crystal
 
 if __name__ == "__main__":
-    crystal = generateTestCrystal()
+    # Test crystal
+    crystal = createCrystal(chem_notation="N", structure="fcc", a=3.6, cubic=True)
     view(crystal)
