b65dc6f77bee5d2b2306c50346356b32ce2ff00c
trastock <emial177@student.liu.se>
2025-09-25 15:16:35 +0200

    Testing CI

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
 import pytest
-if __name__ == "__main__":
-    # Run all tests in ./tests
-    pytest.main(["tests"])
+# if __name__ == "__main__":
+# Run all tests in ./tests
+pytest.main(["tests"])
