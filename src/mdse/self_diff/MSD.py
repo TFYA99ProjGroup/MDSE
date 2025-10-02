16ddf02469cd25ad12dc0a2ca56753898da498de
luksm770 <lukas@lukassmith.se>
2025-10-02 19:52:38 +0200

    Can be called with array

--- src/mdse/self_diff/MSD.py
+++ src/mdse/self_diff/MSD.py
@@ -2,9 +2,12 @@ from ase.io import Trajectory
 import numpy
 from ase import units
 
-def calc_MSD(filename,frames_in_fs = 50):
+def calc_MSD(data,frames_in_fs = 50, to_plot = False):
     """
+    if data is filepath:
     Reads .traj file and calculates the mean-square-displacement
+    if data is array of positions, calculates MSD on that.
+
     MSD = 1/N*|ri(t+tau)-ri(t+tau)|^2
     For each tau/lag/deltat it looks at all frames saved, for all molecules.
 
@@ -25,41 +28,51 @@ def calc_MSD(filename,frames_in_fs = 50):
     TODO: Use vectors in numpy instead for nested for-loops
           !!Unwrap positions because of PBC!!
     """
-    my_traj = Trajectory(filename)
 
-    #Number of frames saved
-    frames = len(my_traj)
+    if isinstance(data,str): #A filepath
+        my_traj = Trajectory(data)
 
-    #MSD has alot of noice in beggning => choose larger starting point
-    #Also noice at the end, so cut-off tau
-    tau_start = 9
-    tau_end = frames -8
+        #Number of frames saved
+        frames = len(my_traj)
+        #Start by extracting all positions
+        positions = []
+        for frame in my_traj:
+            positions.append(frame.positions) #Gives ALL atoms in a frame
+        #positions[t] gives positions ALL atoms at time t
+        #positions[t][i] gives position of atom i, at time t
+        #positions[t][i][0] gives position of atom i in x-direction, at time t
 
+        #Convert to numpy array
+        positions = numpy.array(positions)
+
+        
+        #MSD has alot of noice in beggning => choose larger starting point
+        #Also noice at the end, so cut-off tau
+        tau_start = 9
+        tau_end = frames -8
+
+        #Number of particles
+        N = len(my_traj[0])
+
+
+    elif isinstance(data,numpy.ndarray): #Array with positions
+        positions = data
+        frames = len(positions)
+
+        #Only used if testing, hence must use low tau
+        tau_start = 0
+        tau_end = frames
+
+        N = len(positions[0])
 
     #Number of time-lag (tau) we whant to do
     taus = []
     for a in range(tau_start,tau_end): #Runs faster the fewer taus
         taus.append(a)
 
-    #Number of particles
-    N = len(my_traj[0])
-
-
     MSD_at_tau_x = []
     MSD_at_tau_y = []
     MSD_at_tau_z = []
-
-
-    #Start by extracting all positions
-    positions = []
-    for frame in my_traj:
-        positions.append(frame.positions) #Gives ALL atoms in a frame
-    #positions[t] gives positions ALL atoms at time t
-    #positions[t][i] gives position of atom i, at time t
-    #positions[t][i][0] gives position of atom i in x-direction, at time t
-
-    #Convert to numpy array
-    positions = numpy.array(positions)
 
     for tau in taus:
         MSD_at_all_t_x = [] #Reset per tau
@@ -97,20 +110,21 @@ def calc_MSD(filename,frames_in_fs = 50):
     taus_fs = [tau * frames_in_fs for tau in taus]
 
     #------Print MDS(tau) vs tau
-    import matplotlib.pyplot as plt
+    if (to_plot):
+        import matplotlib.pyplot as plt
 
-    plt.figure(figsize=(6,4))
-    plt.plot(taus_fs, MSD_at_tau_x, label='MSD X', marker='o')
-    plt.plot(taus_fs, MSD_at_tau_y, label='MSD Y', marker='s')
-    plt.plot(taus_fs, MSD_at_tau_z, label='MSD Z', marker='^')
+        plt.figure(figsize=(6,4))
+        plt.plot(taus_fs, MSD_at_tau_x, label='MSD X', marker='o')
+        plt.plot(taus_fs, MSD_at_tau_y, label='MSD Y', marker='s')
+        plt.plot(taus_fs, MSD_at_tau_z, label='MSD Z', marker='^')
 
-    plt.xlabel('Time lag τ (fs)')
-    plt.ylabel('MSD (?²)')
-    plt.title('Mean Squared Displacement vs Time Lag')
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
+        plt.xlabel('Time lag τ (fs)')
+        plt.ylabel('MSD (?²)')
+        plt.title('Mean Squared Displacement vs Time Lag')
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
     #----------------
 
     return taus_fs, MSD_at_tau_x, MSD_at_tau_y, MSD_at_tau_z
