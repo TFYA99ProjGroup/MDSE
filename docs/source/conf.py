77ea66059d63ce3d442f5f9ee1523bb8c285a214
Moderabo <patrik@modorato.se>
2025-10-16 15:25:39 +0200

    Small config changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,10 @@ release = '0.1.0'
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',  # For Google and NumPy style docstrings
-    'sphinx.ext.viewcode'   # Optional: to include links to source code
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',  # for Google/NumPy style docstrings
 ]
+autosummary_generate = True
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -30,5 +31,14 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': False,
+    'private-members': False,
+    'special-members': False,
+    'inherited-members': False,
+    'show-inheritance': True,
+}
