21c97cb476e2a0b45d84c994579c2f7b0a2b1681
OLGBollner <oskar.bollner@gmail.com>
2025-10-28 14:55:31 +0100

    Added comments at top of examples

--- examples/mini_simulation.py
+++ examples/mini_simulation.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env python3
+"""
+Runs a single simulation of fcc copper at 800 K.
+Also views the crystal structure of the simulation object.
+"""
 
-from mdse.md.simulation import simulate_nve, create_crystal
-from ase import units
-from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
-from asap3 import EMT
-# from ase.visualize import view
-# from ase.build import bulk
+from mdse.md.simulationmanager import SimulationManager
 
-# Cubic non-prim unit cell
-test_crystal = create_crystal(chem_notation="Cu", structure="fcc", a=3.6, cubic=True)
-super_cube = test_crystal * (4, 4, 4)
+config = {
+    "Type": "Cu",
+    "Structure": "fcc",
+    "Lattice_a": 3.6,
+    "Cubic": True,
+    "Temp": 800,
+    "Timestep": 2,
+    "Length": 400,
+    "TrajInterval": 10,
+}
 
-MaxwellBoltzmannDistribution(super_cube, temperature_K=800)
-
-super_cube.calc = EMT()
-simulate_nve(super_cube, timestep=2 * units.fs, length=400)
+simulation = SimulationManager(config=config)
+simulation.simulate_nve()
+simulation.view_super_crystal()
--- examples/msd_example.py
+++ examples/msd_example.py
@@ -1,3 +1,7 @@
+"""
+This example utilises the resultMD class to calculate the mean square
+displacement of a simulation.
+"""
 from mdse.md.resultMD import ResultMD
 
 result = ResultMD.from_file("Cu.traj")
