e890eb3523c53814ea9034c62d92f0348cec3510
OLGBollner <oskar.bollner@gmail.com>
2025-10-29 14:46:05 +0100

    Added possibility to use multiple calculators

--- tests/test_mdobject.py
+++ tests/test_mdobject.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import numpy as np
+from pytest import raises
 from mdse.md.simulationmanager import SimulationManager
 
 
@@ -42,3 +44,43 @@ def test_mdobject():
     assert Path.exists(traj_file)
     traj_file.unlink()
 
+def test_simulate_functions():
+    calculator = "EMT"
+
+    sim = {
+        "Type": "Ni",
+        "Structure": "fcc",
+        "Lattice_a": 3.6,
+        "Cubic": True,
+        "Temp": 400,
+        "Timestep": 2,
+        "Length": 800,
+        "TrajInterval": 10,
+    }
+
+    sim1 = SimulationManager(sim)
+
+    sim1.simulate_nve(calculator=calculator)
+
+    numbers = list(set(sim1.crystal.numbers))
+    np.random.seed(42)
+    n = len(numbers)
+    sigma = np.random.rand(n)
+    epsilon = np.random.rand(n)
+
+    calc_params = {"elements": numbers,
+                   "sigma": sigma,
+                   "epsilon": epsilon
+                   }
+
+    calculator = "LennardJones"
+
+    sim1.simulate_nve(calculator=calculator, calc_params=calc_params)
+
+    with raises(NotImplementedError,
+                match="Calculator hej123 not implemented, valid calculators are: EMT"):
+        sim1.simulate_nve(calculator="hej123")
+
+    traj_file = Path("Ni_400.traj")
+    assert Path.exists(traj_file)
+    traj_file.unlink()
