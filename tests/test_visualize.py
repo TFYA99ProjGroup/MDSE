c4e48bf8ee29113cb789ee87aedbc0ab0c18a41e
luksm770 <lukas@lukassmith.se>
2025-11-05 13:58:00 +0100

    Added test for invalid properties

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -23,7 +23,7 @@ class MockAtoms:
 @pytest.fixture
 def mock_results():
     res = []
-    for i in range(0,2):
+    for i in range(0,50):
         #np.random.seed(42)
         frames = [MockAtoms(np.random.rand(5, 3), np.random.rand(5, 3)) for _ in range(20)]
         new_frames = ResultMD(frames)
@@ -33,13 +33,34 @@ def mock_results():
     return res
 
 
-
-def test_plot_3d(mock_results):
+"""
+def test_plot_scatter(mock_results):
     #res1 = resultMD.ResultMD.from_file("test.traj")
     #res2 = resultMD.ResultMD.from_file("cu.traj")
 
     vis1 = VisualizeResult(mock_results)
 
-    vis1.plot_MSD()
-    #vis1.plot_3d("self_diff","lindemann","avg_a",size = 40)
+    #vis1.plot_MSD()
+    #vis1.plot_scatter("self_diff","lindemann","avg_a",size = 40)
+    vis1.plot_histogram("self_diff",200)
+"""
 
+def test_invalid_prop_plot_scatter(mock_results):
+    """Try plotting a scatter with invalid properties
+    """
+    vis = VisualizeResult(mock_results)
+
+    with pytest.raises(RuntimeError):
+        vis.plot_scatter("self_diff","self_diff","avg_dummy")
+    with pytest.raises(TypeError):
+        vis.plot_scatter()
+    with pytest.raises(RuntimeError):
+        vis.plot_scatter("self_diff","avg_dummy","avg_something")
+
+def test_invalid_prop_plot_scatter(mock_results):
+    """Try plotting a histogram with invalid properties
+    """
+    vis = VisualizeResult(mock_results)
+
+    with pytest.raises(RuntimeError):
+        vis.plot_histogram("avg_dummy")
