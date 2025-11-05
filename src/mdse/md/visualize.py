d83e637c52763c0e11120bf2eb3a009d392f456e
luksm770 <lukas@lukassmith.se>
2025-11-05 13:57:26 +0100

    Added histogram plot, logging and error

--- src/mdse/md/visualize.py
+++ src/mdse/md/visualize.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__) #Need to attach Handler?
 
 class VisualizeResult:
     """Class for visualizing ResultMD objects
@@ -13,11 +16,13 @@ class VisualizeResult:
             data (list): List containing ResultMD objects that are to be visualized.
         """
         self.results = data
+        logger.debug(f"Initialized a VisualizeResult object with {len(self.results)} simulations")
 
     def plot_MSD(self):
         """Collects all MSD vs tau for the list of resultMD. Then plots them togheter
         
         """
+        logger.debug(f"Starting to plot MSD for {len(self.results)} simulations")
         colors = cm.viridis(np.linspace(0,1,len(self.results)))
 
         for i, result in enumerate(self.results):
@@ -25,6 +30,7 @@ class VisualizeResult:
             plt.plot(MSD_vs_tau[0],MSD_vs_tau[1],label = f"MSD_x ({result.name})", marker = "o", color = colors[i] )
             plt.plot(MSD_vs_tau[0],MSD_vs_tau[2],label = f"MSD_y ({result.name})", marker = "x", color = colors[i] )
             plt.plot(MSD_vs_tau[0],MSD_vs_tau[3],label = f"MSD_z ({result.name})", marker = "^", color = colors[i] )
+        logger.debug("Sucessfully got MSD data to plot")
 
         plt.xlabel("Time lag (fs)")
         plt.ylabel("MSD (Å²)")
@@ -41,19 +47,46 @@ class VisualizeResult:
             prop3 (str): Property on color scaling.
             size (int): Size of data points
         """
+        logger.debug(f"Starting to plot scatter for {len(self.results)} simulations")
         available = {"self_diff" : "calc_self_diff", "lindemann" : "calc_lindemann", "debye" : "calc_debye_temperature",
                      "avg_a" : "estimate_average_a"}
+        properties = [prop1,prop2,prop3]
 
-        #Error handling needs to be done
+        missing = [p for p in properties if p not in available]
 
+        if missing:
+            logger.debug(f"Was not able to scatter plot, invalid properties given, {missing}")
+            raise RuntimeError(f"Invalid properties {missing}")
+        logger.debug("Scatter plot got valid properties")
 
         x_values = [getattr(result,available[prop1])() for result in self.results]
         plt.xlabel(prop1)
         y_values = [getattr(result,available[prop2])() for result in self.results]
         plt.ylabel(prop2)
         z_values = [getattr(result,available[prop3])() for result in self.results]
+        logger.debug("Scatter plot got data for properties sucesfully")
 
         plt.scatter(x_values, y_values, s= size, c = z_values)
+        plt.colorbar(label = prop3)
 
         plt.show()
 
+    def plot_histogram(self,prop1, bins = 100):
+        """Plots a histogram    
+        """
+        available = {"self_diff" : "calc_self_diff", "lindemann" : "calc_lindemann", "debye" : "calc_debye_temperature",
+                     "avg_a" : "estimate_average_a"}
+
+        if prop1 not in available:
+            logger.debug(f"Was not able to histogram plot, invalid property given, {prop1}")
+            raise RuntimeError(f"Invalid property {prop1}")
+        
+        logger.debug("Histogram plot got valid properties")
+
+        x_values = [getattr(result,available[prop1])() for result in self.results]
+        plt.xlabel(prop1)
+        plt.ylabel("Count")
+        plt.hist(x_values)
+        plt.title(f"Histogram over {prop1}, from {len(self.results)} simulations")
+
+        plt.show()
