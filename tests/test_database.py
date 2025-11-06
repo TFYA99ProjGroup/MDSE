d6f0115bc0e6395095f4fd38b269240d1ca0a285
Moderabo <patrik@modorato.se>
2025-11-06 16:00:14 +0100

    First test

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 
-# Connect to MongoDB on localhost
-client = MongoClient("mongodb://localhost:27017/")
 
-# Test the connection
-print(client.list_database_names())
+def test_set_up_database():
+    # Connect to MongoDB on localhost
+    client = MongoClient("mongodb://localhost:27017/")
