a7331ecc070a78f05d60a5bb045ffbf9909e67d5
Moderabo <patrik@modorato.se>
2025-11-06 16:05:04 +0100

    Functional first test

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,4 +3,6 @@ from pymongo import MongoClient
 
 def test_set_up_database():
     # Connect to MongoDB on localhost
+    client = None
     client = MongoClient("mongodb://localhost:27017/")
+    assert client is not None
