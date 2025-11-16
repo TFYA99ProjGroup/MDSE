1eadc4a13507f3a986d29305930abea752d0aef8
OLGBollner <oskar.bollner@gmail.com>
2025-11-16 15:06:29 +0100

    Converts sqlite crystal data to ase atoms object

---
+++ examples/httk_example.py
@@ -0,0 +1,15 @@
+"""
+This example showcases how to load a crystal structure from an sqlite
+    database into an ASE `Atoms` object.
+    Creates a crystal for the NV center in diamond.
+"""
+
+from ase import Atoms
+from mdse.parser.httk_reader import load_defect_as_ase, setup_db
+from mdse.parser.classes import DefectCell
+
+# This example needs a path to a sqlite database
+search = setup_db("../defects.sqlite")
+
+atoms = load_defect_as_ase("-4871140043124584231", search, DefectCell)
+assert type(atoms) is Atoms
