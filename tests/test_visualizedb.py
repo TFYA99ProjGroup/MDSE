9897f607cfee9565f52375cc1e55b16e99a2d54f
luksm770 <lukas@lukassmith.se>
2025-11-20 17:48:23 +0100

    Test to see that structure of config and data generates correct amount of plots

---
+++ tests/test_visualizedb.py
@@ -0,0 +1,84 @@
+##Get errors from imports. Temporary remove them until can figure out.------------
+import sys
+from types import ModuleType
+
+# Fake heavy modules
+sys.modules['mace'] = ModuleType('mace')
+sys.modules['mace.calculators'] = ModuleType('calculators')
+sys.modules['mace.calculators'].MACECalculator = lambda *args, **kwargs: None
+
+sys.modules['bson'] = ModuleType('bson')
+
+# Fake pymongo and its submodules
+sys.modules['pymongo'] = ModuleType('pymongo')
+sys.modules['pymongo.errors'] = ModuleType('errors')
+sys.modules['pymongo'].MongoClient = lambda *args, **kwargs: None
+sys.modules['pymongo.errors'].ServerSelectionTimeoutError = type(
+    'ServerSelectionTimeoutError', (Exception,), {}
+)
+
+####Remove above---------------------------------------------------------------
+
+from mdse.visualizeDB.run_visDB import run_visualize_db
+import pytest
+from unittest.mock import MagicMock
+
+@pytest.fixture
+def mock_config_data():
+    """Create mock dictionary of data in config .yaml file
+    """
+    data = {"data" : {"data_source" : "json", "path" : "temp_path"},
+            "plots" : {"plot_1" : {"type" : "doping", "average" : True, "fix_y" : True},
+                       "plot_2" : {"type" : "scatter", "x" : "temperature", "y" : "MSD", "z" : "Lindeman"},
+                        "plot_3" : {"type" : "ALL_PLOT", "x" : "temperature", "y" : "MSD", "z" : "Lindeman"},
+                         "plot_4" : {"type" : "scatter", "x" : "speed_per_kelvin", "y" : "MSD", "z" : "Lindeman"} }}
+    
+    return data
+
+
+@pytest.fixture
+def mock_sim_data():
+    """Create mock .json data"""
+    data = [
+        {"doping": "H",  "avg_a": 1.01, "formation_energy": 1.01,
+         "temperature": 300, "MSD": 0.01, "Lindeman": 0.05},
+        {"doping": "H",  "avg_a": 1.05, "formation_energy": 1.03,
+         "temperature": 310, "MSD": 0.02, "Lindeman": 0.06},
+        {"doping": "He", "avg_a": 0.99, "formation_energy": 1.80,
+         "temperature": 300, "MSD": 0.015, "Lindeman": 0.055},
+        {"doping": "Li", "avg_a": 0.92, "formation_energy": 1.11,
+         "temperature": 305, "MSD": 0.012, "Lindeman": 0.052},
+        {"doping": "Cu", "avg_a": 1.02, "formation_energy": 1.02,
+         "temperature": 300, "MSD": 0.02, "Lindeman": 0.05},
+    ]
+
+    return data
+
+
+def test_run_visualize(monkeypatch, mock_config_data, mock_sim_data):
+    """Test that above configurations/structure is valid.
+    And generates plots.
+    Data had 4 plots, 2 which are invalid format. Check so only 2 gets created.
+    """
+
+    #Patch read_yaml and read_data in run_vis to use above data.
+    monkeypatch.setattr("mdse.visualizeDB.run_visDB.read_yaml_simulations",
+                        lambda path: mock_config_data)
+    monkeypatch.setattr("mdse.visualizeDB.run_visDB.read_data",
+                        lambda data_info: mock_sim_data)
+    
+    #Patch so dont greate and save figures.
+    dummy_savefig = MagicMock()
+    monkeypatch.setattr("matplotlib.pyplot.Figure.savefig", dummy_savefig)
+    monkeypatch.setattr("matplotlib.pyplot.close", lambda *args, **kwargs: None)
+
+    run_visualize_db("dummy")
+
+    assert(dummy_savefig.call_count == 2)
+
+    saved_files = [call.args[0] for call in dummy_savefig.call_args_list]
+
+    assert "plot_1.png" in saved_files
+    assert "plot_2.png" in saved_files
+    assert "plot_3.png" not in saved_files
+    assert "plot_4.png" not in saved_files
