f11128838dbb1478692234627bd8e304be397e86
Moderabo <patrik@modorato.se>
2025-12-10 17:36:45 +0100

    Even more docstrings

--- src/mdse/visualizeDB/make_plot.py
+++ src/mdse/visualizeDB/make_plot.py
@@ -4,6 +4,13 @@
 # For a copy, see <https://github.com/TFYA99ProjGroup/MDSE/blob/main/LICENSE>.
 
 
+"""This module provides the main entry point for creating various plots.
+
+It contains a dispatcher function that selects the appropriate plotting
+function based on the configuration and executes it with the provided data.
+"""
+
+
 import logging
 from mdse.visualizeDB.vis_plots import (scatter_plot, doping_plot,
 heatmap_plot, single_defect_plot)
@@ -14,16 +21,18 @@ logger = logging.getLogger(__name__)
 
 
 def make_plot(plot_name, plot_info, sim_data):
-    """Takes information about a plot, and makes it.
-    If unsuccesfull, i.e bad information etc, should simply continue with next plot.
-    The 'main' plotting function.
+    """Takes information about a plot and creates it.
 
-    args:
-        plot_name(str): The name of the plot
-        plot_info(dict): Contains what type of plot, parameters etc.
-        sim_data(dict): The data from simulations. What is getting plotted
+    This function serves as the main entry point for plotting. It attempts to
+    create a plot based on the provided information. If it fails (e.g., due to
+    bad information), it logs the error and continues without crashing.
+
+    Args:
+        plot_name (str): The name of the plot, used for saving the file.
+        plot_info (dict): Contains plot-specific information, such as the plot
+            type and other parameters.
+        sim_data (list[dict]): The simulation data to be plotted.
     """
-
 
     try:
         plot_func = valid_plot(plot_info.get("type"))
@@ -38,15 +47,16 @@ def make_plot(plot_name, plot_info, sim_data):
 
 
 def valid_plot(plot_type):
-    """Checks that the type of plot is valid and supported.
-    If not, will raise error. If good, then returns name of corresponding
-    plot function.
+    """Checks if the plot type is valid and returns the corresponding plot function.
 
-    args:
-        plot_type(str): The type of plot
+    Args:
+        plot_type (str): The type of plot to validate.
 
-    returns:
-        ?: The name of the function that plots this type
+    Returns:
+        callable: The function that creates the specified plot type.
+
+    Raises:
+        ValueError: If the plot_type is not supported.
     """
 
     available_types = {"scatter" : scatter_plot, "doping" : doping_plot,
@@ -57,4 +67,3 @@ def valid_plot(plot_type):
         raise ValueError("The plot is not supported")
 
     return available_types[plot_type]
-
