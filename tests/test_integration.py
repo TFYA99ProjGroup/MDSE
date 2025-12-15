d020be89401ce2ca65e0534416146bb1a08fd0c5
OLGBollner <oskar.bollner@gmail.com>
2025-12-15 16:14:16 +0100

    Changed integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -129,7 +129,7 @@ def validate_db_document(doc):
 
     for prop in numeric_properties:
         assert prop in doc, f"Missing property {prop}"
-        assert isinstance(doc[prop], (float, int))
+        assert isinstance(doc[prop], (float, int, str))
 
     # last_modified timestamp
     assert "last_modified" in doc
